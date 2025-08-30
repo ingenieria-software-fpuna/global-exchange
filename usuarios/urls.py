9e07ee2db3892847ab4dc65edfce9f881dfe68fb
Diegao <diegonoguerarec@gmail.com>
2025-08-30 20:40:21 -0300

    Agregar boton de activación y desactivación en la vista de Usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView, UsuarioDeleteView
+from .views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView, UsuarioDeleteView, toggle_usuario_status
 
 app_name = 'usuarios' 
 urlpatterns = [
@@ -7,6 +7,9 @@ urlpatterns = [
     path('usuarios/crear/', UsuarioCreateView.as_view(), name='user_create'),
     path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='user_update'),
     path('usuarios/eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='user_delete'),
+    
+    # API AJAX para Usuarios
+    path('usuarios/toggle-status/<int:pk>/', toggle_usuario_status, name='toggle_status'),
 ]
 
 
